37235d86754b522b39487571afd29c28fb41c5d5
timothymichaelcook <tmc295@email.vccs.edu>
2023-04-19 12:35:30 -0400

    Finished editor.js file

---
+++ client/src/js/editor.js
@@ -0,0 +1,39 @@
+// Import methods to save and get data from the indexedDB database in './database.js'
+import { getDb, putDb } from './database';
+import { header } from './header';
+
+export default class {
+  constructor() {
+    const localData = localStorage.getItem('content');
+
+    // check if CodeMirror is loaded
+    if (typeof CodeMirror === 'undefined') {
+      throw new Error('CodeMirror is not loaded');
+    }
+
+    this.editor = CodeMirror(document.querySelector('#main'), {
+      value: '',
+      mode: 'javascript',
+      theme: 'monokai',
+      lineNumbers: true,
+      lineWrapping: true,
+      autofocus: true,
+      indentUnit: 2,
+      tabSize: 2,
+    });
+
+    getDb().then((data) => {
+      console.info('Loaded data from IndexedDB, injecting into editor');
+      this.editor.setValue(data || localData || header);
+    });
+
+    this.editor.on('change', () => {
+      localStorage.setItem('content', this.editor.getValue());
+    });
+
+    this.editor.on('blur', () => {
+      console.log('The editor has lost focus');
+      putDb(localStorage.getItem('content'));
+    });
+  }
+}
